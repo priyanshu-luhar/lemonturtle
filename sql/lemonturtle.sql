8d7a8f29cdb28463dc2968bdf9e020629bf98fb9
Priyanshu Luhar <pluhar@artemis.cs.csub.edu>
2025-03-28 01:34:11 -0700

    made a schema and added a database

---
+++ sql/lemonturtle.sql
@@ -0,0 +1,62 @@
+-- **************************************************************************************
+-- SQLITE3 file for Lemon Turtle
+-- Spring 2025
+-- AUTH: Luhar, Priyanshu pluhar@csub.edu
+-- DATE: Mar 27, 2025
+
+-- **************************************************************************************
+-- TABLES
+
+CREATE TABLE IF NOT EXISTS person (
+    userID integer primary key autoincrement,
+    fname text,
+    lname text,
+    email text not null,
+    hash text not null,
+    profileimg text default "../img/default.png",
+    openTime datetime default CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS api (
+    apiNo integer primary key autoincrement,
+    userID integer,
+    url text,
+    response blob default null CHECK (length(response) <= 40000),
+    foreign key (userID) references person(userID) on update cascade on delete set null
+);
+
+CREATE TABLE IF NOT EXISTS usession (
+    sessionID integer primary key autoincrement,
+    userID integer,
+    startTime datetime default CURRENT_TIMESTAMP,
+    endTime datetime default CURRENT_TIMESTAMP,
+    foreign key (userID) references person(userID) on update cascade on delete set null
+);
+
+CREATE TABLE IF NOT EXISTS sessionApi (
+    sessionID integer primary key,
+    apiNo integer,
+    foreign key (sessionID) references usession(sessionID) on update cascade on delete set null
+    foreign key (apiNo) references api(apiNo) on update cascade on delete set null
+
+);
+
+CREATE TABLE IF NOT EXISTS ipMap (
+    ipMapID integer primary key autoincrement,
+    userID integer,
+    url text,
+    ipAddr text,
+    lat real,
+    lng real,
+    hopNo integer,
+    foreign key (userID) references person(userID) on update cascade on delete set null
+);
+
+
+-- **************************************************************************************
+-- DEMO DATA
+
+-- **************************************************************************************
+-- FLAGS
+
+PRAGMA foreign_keys = ON;
